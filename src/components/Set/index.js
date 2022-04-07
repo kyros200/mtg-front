1792edfae85e1ad62e6db89dbe82035727611735
rafael najjar <rafaelnajjar@gmail.com>
2022-04-06 21:51:15 -0300

    partial owned set based on search is now yellow

--- src/components/Set/index.js
+++ src/components/Set/index.js
@@ -5,15 +5,27 @@ import './Set.scss'
 const Set = (props) => {
     const [isOpen, setIsOpen] = useState(false)
 
+    const cardsOwnedThisSet = props?.cards?.reduce((a, v) => {return a + v.have}, 0);
+    const cardsTotalThisSet = props.cards.length;
+    
+    const getSetColor = () => {
+        if(cardsOwnedThisSet === cardsTotalThisSet)
+            return "completed"
+        else if (cardsOwnedThisSet === 0)
+            return ""
+        else
+            return "partial"
+    }
+
     return (
         <div key={props.setId} id={props.setId} className={`set-container`}>
-            <div className={`accodion-button ${props?.cards?.reduce((a, v) => {return a + v.have}, 0) === props.cards.length ? "completed" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+            <div className={`accodion-button ${getSetColor()}`} onClick={() => setIsOpen(!isOpen)}>
                 <div className='set-name'>
                     <img src={props.setIcon} alt={props.setName}/>
                     {props.setName}
                 </div>
                 <div className='quantity'>
-                    {props?.cards?.reduce((a, v) => {return a + v.have}, 0)} / {props.cards.length}
+                    {cardsOwnedThisSet} / {cardsTotalThisSet}
                 </div>
             </div>
             <div className={`accordion-content ${isOpen ? "" : "hide"}`}>
