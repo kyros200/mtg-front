1b27a099e09e94fb023d8af22f6a9a5b1b96d408
rafael najjar <rafaelnajjar@gmail.com>
2022-04-14 01:05:48 -0300

    added customPrice

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import './Card.scss'
 import cardBack from '../../images/card_back.jpg'
 
 const Card = (props) => {
+
+    const [editPanel, setEditPanel] = useState(false)
+    const [alexPrice, setAlexPrice] = useState("")
+
+    const handleCustomPrice = () => {
+        setEditPanel(false)
+        props.setFlags({data: [props.id], priceAlex: alexPrice})
+    }
 
     return (
         <div key={props.id} className={`card-container ${props.have ? "have" : ""}`}>
@@ -11,17 +20,35 @@ const Card = (props) => {
             <div className='image'>
                 <img src={`https://c1.scryfall.com/file/scryfall-cards/normal${props.imageUrl}` || cardBack} alt={props.name} loading="lazy"/>
             </div>
-            <div className='prices'>
-                <div className='price'>
-                    ${props.priceUsd || "-"}
+            <div className='price-container'>
+                <div className={`button`} onClick={() => setEditPanel(!editPanel)}>
+                    E
                 </div>
-            </div>
-            <div className='links'>
-                <a href={`https://www.tcgplayer.com/product/${props.urlTcg}?page=1&utm_campaign=affiliate&utm_medium=api&utm_source=scryfall`} target={"_blank"} rel="noreferrer">
-                    <div className='link'>
-                        TCGPlayer
+                {editPanel ?
+                <>
+                    <input className='input' placeholder='Value...' value={alexPrice} onChange={(e) => setAlexPrice(e.target.value)}/>
+                    <div className={`button`} onClick={() => handleCustomPrice()}>
+                        Save
                     </div>
-                </a>
+                </>
+                :
+                <>
+                    <div className='price'>
+                        ${props.priceAlex ? props.priceAlex.toFixed(2) : props.priceUsd.toFixed(2)}
+                    </div>
+                    {props.priceAlex ?
+                    <div className='link'>
+                            Custom
+                    </div>
+                    :
+                    <div className='link'>
+                        <a href={`https://www.tcgplayer.com/product/${props.urlTcg}?page=1&utm_campaign=affiliate&utm_medium=api&utm_source=scryfall`} target={"_blank"} rel="noreferrer">
+                            TCGPlayer
+                        </a>
+                    </div>
+                    }
+                </>
+                }
             </div>
             <div className={`button ${props.have ? "red" : ""}`} onClick={() => props.setFlags({data: [props.id], have: !props.have})}>
                 {props.have ? "Remove from Collection" : "Add to Collection"}
